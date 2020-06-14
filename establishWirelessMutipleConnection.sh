b9fb1fbc70dbbf254efe4542e996d5a6218fa296
Your Name <you@example.com>
2020-06-14 17:32:03 +0900

    parameter modified

--- establishWirelessMutipleConnection.sh
+++ establishWirelessMutipleConnection.sh
@@ -1,9 +1,12 @@
-SSID="SiteSurvey2"
-# Below NetworkManager audit seems to cause disconnected (reason -3)
-# 6月 01 16:34:37 mdstadmin-UX32VD NetworkManager[842]: <info>  [1590996877.3425] audit: op="connection-activate" uuid="d7a9497c-ef6e-4fd3-9c48-bec160490e08" name="SiteSurvey2 26" pid=8546 uid=0 resul
-# To avoid this error
+SSID="GL-AR750S-a6a-Guest"
+# File descriptor may hit the ceiling.
+# sudo nano /etc/systemd/system/dbus-fi.w1.wpa_supplicant1.service 
+# [Service]
+# LimitNOFILE=4096 <- add
 
-for i in $(seq -w 16 216); do
+# creating too many (more than 140) vif at once, may cause buffer overflow of wpa_supplicant
+
+for i in $(seq -w 16 150); do
     echo "vwlan$i"
     hex=$(echo "obase=16; $i" | bc)
     echo "aa:bb:cc:dd:ee:"$hex
@@ -11,13 +14,12 @@ for i in $(seq -w 16 216); do
 done
 # After interface activation, it requires about 9 seconds to connect to an AP
 sleep 10
-for i in $(seq -w 16 216); do
-    #error handling try 3 times
+for i in $(seq -w 16 150); do
     sudo nmcli device wifi connect $SSID ifname vwlan$i &
     echo "exit with "$?
-    sleep 2
+    sleep 4
 done
 # to delete interface `sudo iw dev vwlan0 del`
-
 # write out csv to be used in JMeter
+sleep 10
 ip -4 a | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | grep -v "127.0.0.1" > ipaddr.csv

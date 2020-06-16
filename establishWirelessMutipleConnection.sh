7a8e64f11dfc6cd8639c82ae047f7987668021ad
Your Name <you@example.com>
2020-06-17 00:22:59 +0900

    considered wpa nl80211 driver timeout 10 sec

--- establishWirelessMutipleConnection.sh
+++ establishWirelessMutipleConnection.sh
@@ -7,26 +7,21 @@ SSID="GL-AR750S-a6a-Guest"
 # creating too many (more than 140) vif at once, may cause buffer overflow of wpa_supplicant
 date
 sudo systemctl stop NetworkManager
-for i in $(seq -w 16 150); do
+for i in $(seq -w 16 50); do
     echo "vwlan$i"
     hex=$(echo "obase=16; $i" | bc)
     echo "aa:bb:00:dd:ee:"$hex
     sudo iw phy0 interface add vwlan$i type managed addr aa:bb:00:dd:ee:$hex
 done
 # After interface activation, it requires about 9 seconds to connect to an AP
-sleep 2
-for i in $(seq -w 16 150); do
-    sudo wpa_supplicant -B -i vwlan$i -c ./wpa_supplicant.conf &
-    echo "exit with "$?
-    sleep 0.005
+for i in $(seq -w 16 50); do
+    date
+    sudo wpa_supplicant -i vwlan$i -c ./wpa_supplicant.conf -dddt >> wpa.log &
+    sleep 10
+    sudo dhclient vwlan$i &
+    echo "dhcp on vwlan$i" &
 done
 # to delete interface `sudo iw dev vwlan0 del`
 # write out csv to be used in JMeter
-for i in $(seq -w 16 150); do
-    sudo dhclient vwlan$i &
-    echo "dhcp on vwlan$i"
-    date
-    sleep 10
-done
-sleep 30
+sleep 20
 ip -4 a | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | grep -v "127.0.0.1" > ipaddr.csv
